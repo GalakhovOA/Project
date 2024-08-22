5edc37c32d0eeda03332187e0840589fc6522431
Oleg <galahov-oleg@inbox.ru>
2024-08-22 23:20:43 +0300

    Реализована кнопка входа

--- pythonProject14/myshop/shop/urls.py
+++ pythonProject14/myshop/shop/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart_view'),
     path('1', views.home_page, name='home_page'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
     path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
 
